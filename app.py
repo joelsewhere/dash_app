cd22599a4a8f5c0e5b37f9eb45c1e60274b387b6
joelsewhere <joelsewhere@gmail.com>
2020-05-01 20:53:24 -0500

    Untrack venv

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-### Data
+# Data
 import pandas as pd
 import pickle
-### Graphing
+# Graphing
 import plotly.graph_objects as go
-### Dash
+# Dash
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Output, Input
-## Navbar
+# Navbar
 from navbar import Navbar
-df = pd.read_csv('https://raw.githubusercontent.com/jayohelee/dash-tutorial/master/data/population_il_cities.csv')
-df.set_index(df.iloc[:,0], drop = True, inplace = True)
-df = df.iloc[:,1:]
+df = pd.read_csv('https://gist.githubusercontent.com/joelsewhere/f75da35d9e0c7ed71e5a93c10c52358d/raw/d8534e2f25495cc1de3cd604f952e8cbc0cc3d96/population_il_cities.csv')
+df.set_index(df.iloc[:, 0], drop=True, inplace=True)
+df = df.iloc[:, 1:]
 
 nav = Navbar()
 
@@ -21,17 +21,19 @@ header = html.H3(
     'Select the name of an Illinois city to see its population!'
 )
 
-options = [{'label':x.replace(', Illinois', ''), 'value': x} for x in df.columns]
+options = [{'label': x.replace(', Illinois', ''), 'value': x}
+           for x in df.columns]
 
 dropdown = html.Div(dcc.Dropdown(
-    id = 'pop_dropdown',
-    options = options,
-    value = 'Abingdon city, Illinois'
+    id='pop_dropdown',
+    options=options,
+    value='Abingdon city, Illinois'
 ))
 
-output = html.Div(id = 'output',
-                children = [],
-                )
+output = html.Div(id='output',
+                  children=[],
+                  )
+
 
 def App():
     layout = html.Div([
@@ -42,19 +44,19 @@ def App():
     ])
     return layout
 
-def build_graph(city):
-    data = [go.Scatter(x = df.index,
-                        y = df[city],
-                        marker = {'color': 'orange'})]
-    graph = dcc.Graph(
-           figure = {
-               'data': data,
-               'layout': go.Layout(
-                    title = '{} Population Change'.format(city),
-                    yaxis = {'title': 'Population'},
-                    hovermode = 'closest'
-                                  )
-                       }
-             )
-    return graph
 
+def build_graph(city):
+    data = [go.Scatter(x=df.index,
+                       y=df[city],
+                       marker={'color': 'orange'})]
+    graph = dcc.Graph(
+        figure={
+            'data': data,
+            'layout': go.Layout(
+                title='{} Population Change'.format(city),
+                yaxis={'title': 'Population'},
+                hovermode='closest'
+            )
+        }
+    )
+    return graph
